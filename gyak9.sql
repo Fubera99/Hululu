363638cd02982100be7b67904040a7e31902e78f
Barnabás Kiss <73836197+Fubera99@users.noreply.github.com>
2022-04-06 11:34:38 +0200

    sql9.gyak vége

--- gyak9.sql
+++ gyak9.sql
@@ -1,4 +1,8 @@
 
+
+/*
+--- ---
+
 declare
     type rek_type is record(f1 integer default 10, f2 vzoli.osztaly%ROWTYPE);
     rec rek_type;
@@ -21,11 +25,8 @@ begin
     end loop;
 end;
 
+--- ---
 
-    
-
-
-/*
 accept v1 number format '99' prompt 'adja meg';
 accept v2 date format 'yyyy.mm.dd' default '1982.01.01' prompt 'Dátum:';
 
@@ -38,9 +39,9 @@ begin
     select sum(fizetes) into v_osszeg from vzoli.dolgozo where oazon = &v1 and belepes < to_date('&v2');
     dbms_output.put_line(v_osszeg);
 end;
-*/
 
-/*
+--- ---
+
 declare
     type rektip is record (m1 integer, m2 varchar(10));
     rec rektip;
@@ -49,9 +50,46 @@ begin
     rec.m2 := 'Bubu';
     dbms_output.put_line(rec.m2);
 end;
+
+--- explicit cursor deklaráció ---
+
+declare
+    cursor c_curs1(oa number) is select oazon, dnev from vzoli.dolgozo where oazon = oa;
+    rec c_curs1%ROWTYPE;
+begin
+    open c_curs1(10);
+    loop
+        fetch c_curs1 into rec;
+        exit when c_curs1%NOTFOUND;
+        dbms_output.put_line(to_char(rec.oazon) || ' - ' || rec.dnev);
+    end loop;
+    close c_curs1;
+end;
+
+--- cursor for loop-pal ---
+declare
+    cursor c_curs1(oa number) is select oazon, dnev from vzoli.dolgozo where oazon = oa;
+begin
+     for rec in c_curs1(10) loop   
+        exit when c_curs1%NOTFOUND;
+        dbms_output.put_line(to_char(rec.oazon) || ' - ' || rec.dnev);
+    end loop;
+end;
+
+--- faktoriális ---
+create or replace function f_nfact(n number) return number
+    is
+    begin
+        if n < 0 then
+            return -1;
+        elsif n > 1 then
+            return n*f_nfact(n-1);
+        else
+            return 1;
+        end if;
+    end;
+    
 */
-
-
 
 
 
